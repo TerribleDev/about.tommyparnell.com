f740dd06336818b3f59e946baf65dae6076da038
Tommy <parnell.tommy@hotmail.com>
2015-02-09 22:25:52 -0500

    remove github on mobile

---
+++ src/AboutMe.Web/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AboutMe.Web
+{
+    public static class Settings
+    {
+        public static IEnumerable<string> Phones = new[] {"iphone","ppc",
+                                                      "windows ce","blackberry",
+                                                      "opera mini","mobile","palm",
+                                                      "portable","opera mobi", "android" };
+
+        public static bool IsMobile(string userAgent)
+        {
+            if (string.IsNullOrWhiteSpace(userAgent))
+            {
+                return false;
+            }
+            var agent = userAgent.ToLower();
+            return Phones.Any(x => agent.Contains(x));
+        }
+    }
+}
