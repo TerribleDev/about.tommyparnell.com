5068b110949a8986b63c7b3bfabcc76b8e7ca186
Tommy <parnell.tommy@hotmail.com>
2015-02-10 20:39:04 -0500

    include robots

--- src/AboutMe.Web/Modules/Index.cs
+++ src/AboutMe.Web/Modules/Index.cs
@@ -1,4 +1,8 @@
-﻿using Nancy;
+﻿using System;
+using System.IO;
+using System.Text;
+using Nancy;
+using Nancy.Responses;
 
 namespace AboutMe.Web.Modules
 {
@@ -7,6 +11,19 @@ namespace AboutMe.Web.Modules
         public Index()
         {
             Get["/"] = x => View["Index"];
+            Get["robots.txt"] = x =>
+            {
+                var res = this.Response.AsFile("robots.txt", "text/plain");
+                res.Contents = a =>
+                {
+                    var msg = new StreamWriter(a);
+                    msg.Write(new StringBuilder().AppendLine("User-agent: *"));
+                    msg.Flush();
+                    msg.Dispose();
+                };
+
+                return res;
+            };
         }
     }
 }
