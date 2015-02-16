35b36ef88ac09fc2253a2cf2431e46be672769cd
Tommy <parnell.tommy@hotmail.com>
2015-02-15 22:33:44 -0500

    adding insights

--- src/AboutMe.Web/Scripts/main.js
+++ src/AboutMe.Web/Scripts/main.js
@@ -44,7 +44,13 @@ jQuery(document).ready(function ($) {
             entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
         }
     );
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "tparnell8", selector: "#ghfeed" });
 });
+
+var appInsights = window.appInsights || function (config) {
+    function s(config) { t[config] = function () { var i = arguments; t.queue.push(function () { t[config].apply(t, i) }) } } var t = { config: config }, r = document, f = window, e = "script", o = r.createElement(e), i, u; for (o.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", r.getElementsByTagName(e)[0].parentNode.appendChild(o), t.cookie = r.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace"]; i.length;) s("track" + i.pop()); return config.disableExceptionTracking || (i = "onerror", s("_" + i), u = f[i], f[i] = function (config, r, f, e, o) { var s = u && u(config, r, f, e, o); return s !== !0 && t["_" + i](config, r, f, e, o), s }), t
+}({
+    instrumentationKey: "baa24b98-894e-4f64-b451-905c59df52b6"
+});
+
+window.appInsights = appInsights;
+appInsights.trackPageView();
