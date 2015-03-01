4954d7b8b0d8bb23c8b16b2e2cd95978faaa0732
Tommy Parnell <tparnell8@gmail.com>
2015-02-28 21:48:05 -0500

    use iis static module

--- src/AboutMe.Web/AppStart/Bootstrap.cs
+++ src/AboutMe.Web/AppStart/Bootstrap.cs
@@ -5,15 +5,15 @@ namespace AboutMe.Web.AppStart
 {
     public class Bootstrap : DefaultNancyBootstrapper
     {
-        protected override void ConfigureConventions(NancyConventions conventions)
-        {
-            base.ConfigureConventions(conventions);
+        //protected override void ConfigureConventions(NancyConventions conventions)
+        //{
+        //    base.ConfigureConventions(conventions);
 
-            conventions.StaticContentsConventions.AddDirectory("Images");
-            conventions.StaticContentsConventions.AddDirectory("Scripts");
-            conventions.StaticContentsConventions.AddDirectory("fonts");
-            conventions.StaticContentsConventions.AddFile("/robots.txt",System.IO.Path.Combine("Content", "robots.txt"));
-            conventions.StaticContentsConventions.AddFile("/sitemap.xml", System.IO.Path.Combine("Content", "sitemap.xml"));
-        }
+        // conventions.StaticContentsConventions.AddDirectory("Images");
+        //conventions.StaticContentsConventions.AddDirectory("Scripts");
+        //conventions.StaticContentsConventions.AddDirectory("fonts");
+        // conventions.StaticContentsConventions.AddFile("/robots.txt",System.IO.Path.Combine("Content", "robots.txt"));
+        // conventions.StaticContentsConventions.AddFile("/sitemap.xml", System.IO.Path.Combine("Content", "sitemap.xml"));
+        //}
     }
 }
